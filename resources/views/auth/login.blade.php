aa7afe20e24ff8bcebee90652534f8f28a5c3403
YounesChellaf <fy_chellaf@esi.dz>
2020-11-08 14:07:21 +0100

    change the login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,48 @@
 @extends('layouts.login')
-
 @section('content')
-
-<style type="text/css">
-    .form-horizontal .form-group {
-         margin: 0px !important; 
-    }
-</style>
-
-<div class="login-box">
-  <div class="login-logo">
-    <a href="/"><b>Gestion BDV</b></a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">veuillez vous connecter</p>
-
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-      
-      {{ csrf_field() }}
-
-      <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
-            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-            <span class="fa fa-envelope form-control-feedback"></span>
-            @if ($errors->has('email'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('email') }}</strong>
-                </span>
-            @endif
-      </div>
-      
-      <div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
-            <input id="password" type="password" class="form-control" name="password" required>
-            <span class="fa fa-lock form-control-feedback"></span>
-            @if ($errors->has('password'))
-                <span class="help-block">
-                    <strong>{{ $errors->first('password') }}</strong>
-                </span>
-            @endif
-      </div>
-
-      <div class="row">
-            <div class="col-xs-8">
-              <div class="checkbox">
-                    <label>
-                        {{-- <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me --}}
-                    </label>
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="{{ asset('frontend/assets/images/easybusinesssuitelogo.png') }}" alt="">
+                <hr>
+                <img id="SonatrachLogo" src="{{ asset('frontend/assets/images/sonatrach_logo.gif') }}" alt="">
+            </div>
+            <form class="login100-form validate-form" role="form" method="POST" action="{{ url('/login') }}">
+                {{ csrf_field() }}
+                <span class="login100-form-title">
+            Connexion
+          </span>
+                @if(Session::get('wrong_credentials'))
+                    <span class="error" style="color: red; margin-left: 15%;">Authentification échouée</span>
+                @endif
+                <div class="wrap-input100 validate-input" data-validate = "Le compte d'utilisateur est obligatoire">
+                    <input id="email" type="email" class="input100" name="email" value="{{ old('email') }}" required autofocus>
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+              <i class="fa fa-user" aria-hidden="true"></i>
+            </span>
                 </div>
-            </div>
-            <!-- /.col -->
-            <div class="col-xs-4">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">connecter</button>
-            </div>
-            <!-- /.col -->
-      </div>
-
-    </form>
-
-    {{-- <a class="btn btn-link" href="{{ url('/password/reset') }}"> Forgot Your Password? </a> --}}
-
-  </div>
-  <!-- /.login-box-body -->
-</div>
+                <div class="wrap-input100 validate-input" data-validate = "Le mot de passe est obligatoire">
+                    <input id="password" type="password" class="input100" name="password" required>
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+              <i class="fa fa-lock" aria-hidden="true"></i>
+            </span>
+                </div>
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn">
+                        Go
+                    </button>
+                </div>
+                <div class="text-center p-t-12">
+            <span class="txt1">
+              J'ai oublié
+            </span>
+                    <a class="txt2" href="#">
+                        Mon mot de passe
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,29 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>EASY BUSINESS SUITE :: AZIMUT BUSINESS SOLUTIONS</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link href="/css/app.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/AdminLTE.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend/login/css/main.css') }}">
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
 </head>
-<body class="hold-transition login-page">
-    <div id="app">
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+<body>
+<div class="limiter" id="app">
+   @yield('content')
+</div>
+<script src="/js/app.js"></script>
+<script src="{{ asset('frontend/login/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<script src="{{ asset('frontend/login/vendor/bootstrap/js/popper.js') }}"></script>
+<script src="{{ asset('frontend/login/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('frontend/login/vendor/select2/select2.min.js') }}"></script>
+<script src="{{ asset('frontend/login/vendor/tilt/tilt.jquery.min.js') }}"></script>
+<script >
+    $('.js-tilt').tilt({
+        scale: 1.1
+    })
+</script>
+<script src="{{ asset('frontend/login/js/main.js') }}"></script>
 </body>
 </html>
